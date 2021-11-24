2ebaa44b21857d6effe5bdebc5fa08a23a5ecee2
Leah Grim <grimchelate@gmail.com>
2021-11-24 16:38:30 -0600

    added the first stretch goal question

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,8 +2,6 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
-// add a global const maxItems equal to 5
-const maxItems = 5;
 // set basket equal to an empty array
 let gemList= ['opal', 'ruby', 'topaz'];
 let basket= [];
@@ -16,7 +14,6 @@ console.log('in addItem:', addItem('Ruby'));
 //adding an extra item to the list for good measure
 console.log('in addItem', addItem('Emerald'));
 console.log(basket);
-
 
 //console.log('checking if our basket includes Opal', basket.includes('Opal'));
 //console.log('checking if our basket includes Ruby', basket.includes('Ruby'));
@@ -38,3 +35,24 @@ function emptyArray(array){
   return array
 }
 console.log(emptyArray(basket));
+
+// STRETCH GOALS//
+// add a global const maxItems equal to 5
+//create function called isFull
+const maxItems = 5;
+function isFull(array, thing1) {
+    if (array.length < maxItems) { array.push(thing1);
+  } else {
+    console.log('no more room for', thing1);
+  }
+  return true;
+}
+// calling the function to evaluate each string
+isFull(basket, 'apples');
+isFull(basket, 'oranges');
+isFull(basket, 'grapefruits');
+isFull(basket, 'celery');
+isFull(basket, 'cranberries');
+isFull(basket, 'thyme');
+isFull(basket, 'oregano');
+console.log(basket);
